9f9323eab5b11a40fefd21782befc8d8b70048cd
Felix <felixkruger13@gmail.com>
2021-04-28 20:58:11 +0200

    1

---
+++ JoinMe/src/main/java/de/sandmc/joinme/Main.java
@@ -0,0 +1,13 @@
+package de.sandmc.joinme;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    @Override
+    public void onEnable() {
+        Plugin plugin = this;
+
+
+    }
+}
